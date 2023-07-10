8e854976a6e45c10247fac3a53f0833a141d50ed
bretton7700 <gitongabretton@gmail.com>
2023-07-10 16:47:48 +0300

    grouping tests

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,16 +1,20 @@
 import { render,screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test('Greet renders correctly',()=>{
-    render(<Greet/>)
-    const expectedText =screen.getByText(/hello/i)
-    expect(expectedText).toBeInTheDocument();
 
-})
+describe('Greet', ()=>{
+    test('Greet renders correctly',()=>{
+        render(<Greet/>)
+        const expectedText =screen.getByText(/hello/i)
+        expect(expectedText).toBeInTheDocument();
+    
+    })
+    
+    
+    test('Greet renders with a name', ()=>{
+        render(<Greet name="Bretton"/>)
+        const expectedText = screen.getByText(/Hello Bretton/i)
+        expect(expectedText).toBeInTheDocument();
+    })
 
-
-test('Greet renders with a name', ()=>{
-    render(<Greet name="Bretton"/>)
-    const expectedText = screen.getByText(/Hello Bretton/i)
-    expect(expectedText).toBeInTheDocument();
 })
