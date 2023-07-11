40ae91cacf764a0bf50eb18f6500b3f1be9a39c0
bretton7700 <gitongabretton@gmail.com>
2023-07-11 09:57:29 +0300

    getByPlaceHolder

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -24,6 +24,9 @@ describe("Application",()=>{
         })
         expect(nameElement2).toBeInTheDocument();
 
+        const nameElement3 = screen.getByPlaceholderText("FullName")
+        expect(nameElement3).toBeInTheDocument();
+
         const bioElement = screen.getByRole("textbox",{
             name:"Bio"
         })
